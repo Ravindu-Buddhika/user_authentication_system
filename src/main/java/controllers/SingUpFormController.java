92f62fb22b49a5283f49ce8da030970cae2a9562
Ravindu-Buddhika <05ravindubuddhika@gmail.com>
2026-01-13 13:54:04 +0530

    ui update

--- src/main/java/controllers/SingUpFormController.java
+++ src/main/java/controllers/SingUpFormController.java
@@ -4,6 +4,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import model.dto.UserDTO;
 
 public class SingUpFormController {
 
@@ -18,6 +19,9 @@ public class SingUpFormController {
 
     @FXML
     private TextField txtSecondName;
+
+    @FXML
+    private TextField txtEmail;
 
     @FXML
     void clickedSingUp(ActionEvent event) {
