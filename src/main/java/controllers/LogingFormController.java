231b237c362147d8704755a39aa93db39213c995
Ravindu-Buddhika <05ravindubuddhika@gmail.com>
2026-01-13 10:28:21 +0530

    make runing state singUpForm

--- src/main/java/controllers/LogingFormController.java
+++ src/main/java/controllers/LogingFormController.java
@@ -2,11 +2,19 @@ package controllers;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class LogingFormController {
+    Stage stage=new Stage();
 
     @FXML
     private TextField txtEmail;
@@ -16,7 +24,14 @@ public class LogingFormController {
 
     @FXML
     void clickedCreateAnAccount(MouseEvent event) {
-
+        stage= (Stage) ((Node) event.getSource()).getScene().getWindow();
+        try {
+            FXMLLoader loader=new FXMLLoader(getClass().getResource("/view/singUpForm.fxml"));
+            Parent root = loader.load();
+            stage.setScene(new Scene(root));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @FXML
