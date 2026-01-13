9e100193c5a46fdb0e2aa09fa987d1c0dbeee053
Ravindu-Buddhika <05ravindubuddhika@gmail.com>
2026-01-13 10:33:50 +0530

    created model class LogingInfo

---
+++ src/main/java/model/LogingInfo.java
@@ -0,0 +1,13 @@
+package model;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class LogingInfo {
+    private String email;
+    private String password;
+}
