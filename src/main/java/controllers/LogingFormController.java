3a30a7021fc0d83763ad5219450dba1e4ad6451c
Ravindu-Buddhika <05ravindubuddhika@gmail.com>
2026-01-13 10:03:39 +0530

    Created sing in ui window

--- src/main/java/controllers/LogingFormController.java
+++ src/main/java/controllers/LogingFormController.java
@@ -1,4 +1,27 @@
 package controllers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+
 public class LogingFormController {
+
+    @FXML
+    private TextField txtEmail;
+
+    @FXML
+    private PasswordField txtPassword;
+
+    @FXML
+    void clickedCreateAnAccount(MouseEvent event) {
+
+    }
+
+    @FXML
+    void clickedSingIn(ActionEvent event) {
+
+    }
+
 }
