8c06befd8bf16bd857268104292f1d89c6e9beb1
Ravindu-Buddhika <05ravindubuddhika@gmail.com>
2026-01-13 10:38:33 +0530

    move package to dto

--- src/main/java/model/dto/LogingInfoDTO.java
+++ src/main/java/model/dto/LogingInfoDTO.java
@@ -1,4 +1,4 @@
-package model;
+package model.dto;
 
 import lombok.*;
 
@@ -7,7 +7,7 @@ import lombok.*;
 @AllArgsConstructor
 @NoArgsConstructor
 @ToString
-public class LogingInfo {
+public class LogingInfoDTO {
     private String email;
     private String password;
 }
