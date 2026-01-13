9f0e3c8515786c7e4b3e0d7a3742de1e8515c7c0
Ravindu-Buddhika <05ravindubuddhika@gmail.com>
2026-01-13 13:45:47 +0530

    created loging function

--- src/main/java/repository/IMPL/LogingRepositoryIMPL.java
+++ src/main/java/repository/IMPL/LogingRepositoryIMPL.java
@@ -1,4 +1,34 @@
 package repository.IMPL;
 
-public class LogingRepositoryIMPL {
+import database.DBConnection;
+import model.entity.LogingInfoEntity;
+import repository.LogingRepository;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class LogingRepositoryIMPL implements LogingRepository {
+    Connection connection= DBConnection.getInstance().getConnection();
+
+    public LogingRepositoryIMPL() throws SQLException {
+    }
+
+    @Override
+    public LogingInfoEntity getUser(LogingInfoEntity entity) throws SQLException {
+        LogingInfoEntity logingInfoEntity = null;
+
+        PreparedStatement preparedStatement = connection.prepareStatement(
+                "SELECT * FROM login_info WHERE email = ?"
+        );
+        preparedStatement.setString(1, entity.getEmail());
+        ResultSet resultSet = preparedStatement.executeQuery();
+
+        if (resultSet.next()) {
+            logingInfoEntity.setEmail(resultSet.getString("email"));
+            logingInfoEntity.setPassword(resultSet.getString("password"));
+        }
+        return logingInfoEntity;
+    }
 }
