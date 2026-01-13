39e12b296e9160a2be5a268cee7e23eeb489ff12
Ravindu-Buddhika <05ravindubuddhika@gmail.com>
2026-01-13 10:27:48 +0530

    Created sing up ui window

---
+++ src/main/java/controllers/SingUpFormController.java
@@ -0,0 +1,26 @@
+package controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class SingUpFormController {
+
+    @FXML
+    private PasswordField txtConfirmPassword;
+
+    @FXML
+    private TextField txtFirstName;
+
+    @FXML
+    private PasswordField txtPassword;
+
+    @FXML
+    private TextField txtSecondName;
+
+    @FXML
+    void clickedSingUp(ActionEvent event) {
+
+    }
+}
