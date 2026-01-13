05e39ef1df3e31f4e14c358fa0e0deeae5d0128d
Ravindu-Buddhika <05ravindubuddhika@gmail.com>
2026-01-13 13:45:25 +0530

    created loging entity molde class

---
+++ src/main/java/model/entity/LogingInfoEntity.java
@@ -0,0 +1,13 @@
+package model.entity;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class LogingInfoEntity {
+    private String email;
+    private String password;
+}
